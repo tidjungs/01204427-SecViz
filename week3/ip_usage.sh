fff4da25adf861b58b6c4c5846fe5dbc06b94594
tidjungs <waewprach.s@ku.th>
2018-08-31 12:35:41 +0700

    add frontend visualize

--- week3/ip_usage.sh
+++
@@ -1,3 +0,0 @@
-echo "dual: $(cut -d " " -f 6-7 login-20170102-anon.csv.txt | grep -v "-" | wc -l)"
-echo "ipv6: $(cut -d " " -f 6-7 login-20170102-anon.csv.txt | grep "^-" | wc -l)"
-echo "ipv4: $(cut -d " " -f 6-7 login-20170102-anon.csv.txt | grep "..-" | wc -l)"
--- week3/server.sh
+++
@@ -1,4 +0,0 @@
-for ((i=201; i<=212; i++))
-do
-  echo "$i: $(cut -d " " -f 8 login-20170102-anon.csv.txt | grep ::ffff:158.108.218.$i | wc -l)"
-done
